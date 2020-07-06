b9dbdd5c8b06414a698db3a768c7fccc9668df45
mathewo-dev <mathew.outing@gmail.com>
2020-07-06 10:45:07 +0200

    create app

---
+++ src/components/AppFooter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+function AppFooter() {
+  return (
+    <footer className="App-footer">
+      <h3>This is my footer</h3>
+    </footer>
+  );
+}
+
+export default AppFooter;
---
+++ src/components/AppHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+import MyText from "./MyText";
+
+function AppHeader() {
+  return (
+    <header className="App-header">
+      <MyText />
+    </header>
+  );
+}
+
+export default AppHeader;
