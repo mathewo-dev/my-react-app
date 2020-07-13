4d9c254fc22d2d40a6e9d3b2ef1800999bba5692
mathewo-dev <mathew.outing@gmail.com>
2020-07-13 18:55:24 +0200

    Created cart components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AppFooter from "./components/AppFooter";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import Details from "./components/Details";
 import Cart from "./components/Cart";
 import Default from "./components/Default";
+import Modal from "./components/Modal";
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
         <Route path="/Cart" component={Cart} />
         <Route component={Default} />
       </Switch>
-
+      <Modal />
       <AppFooter />
     </div>
   );
---
+++ src/components/Cart/EmptyCart.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function EmptyCart() {
+  return (
+    <div classname="container mt-5">
+      <div classname="row">
+        <div className="col-10 mx-auto text-center text-title">
+          <h1>your cart is currently empty</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
